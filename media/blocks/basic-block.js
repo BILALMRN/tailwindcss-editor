d27c016bb67267dda37b633b27fed688b40c1be5
BILALMRN <bilalmrn.mail@gmail.com>
2024-11-05 20:08:21 +0100

    feat: added prebuilt components

---
+++ media/blocks/basic-block.js
@@ -0,0 +1,111 @@
+export const basicBlocks = [
+    {
+      "id": "text-block",
+      "label": "Text Block",
+      "content": "<p class='p-4 '>This is a Tailwind text block.</p>",
+      "category": "Basic",
+      "attributes": {
+        "class": "fa fa-info"
+      }
+    },
+    {
+      "id": "link-block",
+      "label": "Link Block",
+      "content": "<a href='#' class='text-blue-500 hover:underline'>This is a link</a>",
+      "category": "Basic",
+      "attributes": {
+        "class": "fa fa-link"
+      }
+    },
+    {
+      "id": "image-block",
+      "label": "Image Block",
+      "content": "<img src='https://grapesjs.com/assets/images/sc-grapesjs-newsletter-demo.jpg' alt='Placeholder Image' class='w-full h-auto'/>",
+      "category": "Basic",
+      "attributes": {
+        "class": "fa fa-image"
+      }
+    },
+    {
+      "id": "video-block",
+      "label": "Video Block",
+      "content": "<video controls class='w-full'><source src='video.mp4' type='video/mp4'>Your browser does not support the video tag.</video>",
+      "category": "Basic",
+      "attributes": {
+        "class": "fa fa-play"
+      }
+    },
+    {
+      "id": "youtube-block",
+      "label": "YouTube Video Block",
+      "content": "<iframe class='w-full h-64' src='https://www.youtube.com/embed/' frameborder='0' allowfullscreen></iframe>",
+      "category": "Basic",
+      "attributes": {
+        "class": "fa fa-youtube"
+      }
+    },
+    {
+      "id": "section-block",
+      "label": "Section Block",
+      "content": "<section class='p-8 bg-gray-100'><h2 class='text-2xl font-bold'>Section Title</h2><p class='text-gray-700'>This is a section block.</p></section>",
+      "category": "Basic",
+      "attributes": {
+        "class": "fa fa-align-justify"
+      }
+    },
+    {
+      "id": "quote-block",
+      "label": "Quote Block",
+      "content": "<blockquote class='border-l-4 border-blue-500 pl-4 italic'>This is a quote.</blockquote>",
+      "category": "Basic",
+      "attributes": {
+        "class": "fa fa-quote-left"
+      }
+    },
+    {
+      "id": "Layout-block",
+      "label": "Layout Block",
+      "content": "<div class='p-4'></div>",
+      "category": "Layout",
+      "attributes": {
+        "class": "fa fa-square"
+      }
+    },
+    {
+      "id": "2columns-block",
+      "label": "2 Columns Block",
+      "content": "<div class='grid grid-cols-2 gap-4'><div class='p-4'></div><div class='p-4'></div></div>",
+      "category": "Layout",
+      "attributes": {
+        "class": "fa fa-columns"
+      }
+    },
+    {
+      "id": "grid-3-columns-block",
+      "label": "3 Columns Grid Block",
+      "content": "<div class='grid grid-cols-3 gap-4'><div class='p-4'></div><div class='p-4'></div><div class='p-4'></div></div>",
+      "category": "Layout",
+      "attributes": {
+          "class": "fa fa-th"
+      }
+    },
+    {
+      "id": "flex-block",
+      "label": "Flex Block",
+      "content": "<div class='flex p-4'><div class='flex-1 p-4'></div><div class='flex-1 p-4'></div></div>",
+      "category": "Layout",
+      "attributes": {
+          "class": "fa fa-columns"
+      }
+  },
+  {
+      "id": "flex-wrap-block",
+      "label": "Flex Wrap Block",
+      "content": "<div class='flex flex-wrap p-4'><div class='flex-1 p-4'></div><div class='flex-1 p-4'></div><div class='flex-1 p-4'></div></div>",
+      "category": "Layout",
+      "attributes": {
+          "class": "fa fa-arrows-h"
+      }
+  }
+    
+  ];
---
+++ media/blocks/blocks.js
@@ -0,0 +1,3 @@
+import {blocksFlowbite} from "./flowbite.js"
+import {blocksTailwindui} from "./tailwindui.js"
+export const filesBlocks = [blocksFlowbite,blocksTailwindui];
